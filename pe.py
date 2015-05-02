277002a6c217846f89d100c67bac9ba6a9c6fac3
Michael A. Smith <msmith3@ebay.com>
2015-05-02 16:29:23 -0400

    Prefix/Suffix

--- pe.py
+++ pe.py
@@ -11,12 +11,18 @@ Given a string, expand that string using POSIX [parameter expansion][1].
 - Only ASCII alphanumeric characters and underscores are supported in parameter
 names. (Per POSIX, parameter names may not begin with a numeral.)
 - Assignment expansions do not mutate the real environment.
+- For simplicity's sake, this implementation uses fnmatch instead of
+completely reimplementing [POSIX pattern matching][2]
+- Comments in strings are unsupported.
 
 [1]: http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
+[2]: http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_13
 """
 
+from fnmatch import translate
 from itertools import takewhile
 from os import getenv
+from re import compile
 from shlex import shlex
 
 def expand(s, env=None):
@@ -28,6 +34,7 @@ def expand(s, env=None):
 
 def expand_tokens(s, env):
     shl = shlex(s, posix=True)
+    shl.commenters = ""
     while True:
         # This apparently infinite loop should eventually
         # raise StopIteration and thus exit.
@@ -39,6 +46,31 @@ def follow_sigil(shl, env):
     if param == "{":
         return follow_brace(takewhile(lambda t: t != "}", shl), env)
     return env.get(param, "")
+
+def remove_affix(subst, shl, suffix=True):
+    """
+    http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_13
+    """
+    word = next(shl)
+    max = False
+    if word == "%":
+        word = next(shl)
+        max = True
+    pat = ("" if suffix else "^") + \
+            translate(word).strip("\Z(?ms)") + \
+            ("$" if suffix else "")
+    re = compile(pat)
+    while True:
+        orig = subst
+        subst = re.sub('', subst, 1)
+        if (not max) or orig == subst:
+            return subst
+
+def remove_suffix(subst, shl):
+    return remove_affix(subst, shl, True)
+
+def remove_prefix(subst, shl):
+    return remove_affix(subst, shl, False)
 
 def follow_brace(shl, env):
     param = next(shl)
@@ -52,7 +84,11 @@ def follow_brace(shl, env):
     param_unset = param not in env
     try:
         modifier = next(shl)
-        if modifier == ":":
+        if modifier == "%":
+            return remove_suffix(subst, shl)
+        elif modifier == "#":
+            return remove_prefix(subst, shl)
+        elif modifier == ":":
             modifier = next(shl)
             if modifier == "-":
                 word = next(shl)
