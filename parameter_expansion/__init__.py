606f14e9baf9d4a1fea277731457f071bfca5c30
Michael A. Smith <michael@smith-li.com>
2021-06-27 17:51:33 -0400

    Automate Releasing (#3)

--- parameter_expansion/__init__.py
+++ parameter_expansion/__init__.py
@@ -1,159 +1,50 @@
-#!/usr/bin/env python
+import sys
+import warnings
+from typing import Optional
 
-"""
-Given a string, expand that string using POSIX [parameter expansion][1].
+from .pe import ParameterExpansionNullError, expand
 
-## Limitations
-
-(Pull requests to remove limitations are welcome.)
-
-- Nested expansions `${foo:-$bar}` are not supported.
-- Only ASCII alphanumeric characters and underscores are supported in parameter
-names. (Per POSIX, parameter names may not begin with a numeral.)
-- Assignment expansions do not mutate the real environment.
-- For simplicity's sake, this implementation uses fnmatch instead of
-completely reimplementing [POSIX pattern matching][2]
-- Comments in strings are unsupported.
-
-[1]: http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
-[2]: http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_13
-"""
-
-from fnmatch import fnmatchcase
-from itertools import takewhile
-from os import getenv
-from shlex import shlex
+_default_version = "0.0.0+unknown"
 
 
-def expand(s, env=None):
-    """Expand the string using POSIX parameter expansion rules.
-    Uses the provided environment dict or the actual environment."""
-    if env is None:
-        env = dict(os.environ)
-    return "".join(expand_tokens(s, env))
-
-
-class ParameterExpansionNullError(LookupError):
-    pass
-
-
-class ParameterExpansionParseError(Exception):
-    pass
-
-
-def expand_tokens(s, env):
-    shl = shlex(s, posix=True)
-    shl.commenters = ""
-    while True:
-        try:
-            yield "".join(takewhile(lambda t: t != "$", shl))
-            yield follow_sigil(shl, env)
-        except StopIteration:
-            return
-
-
-def follow_sigil(shl, env):
-    param = next(shl)
-    if param == "{":
-        consume = iter(list(takewhile(lambda t: t != "}", shl)))
-        return follow_brace(consume, env)
-    return env.get(param, "")
-
-
-def remove_affix(subst, shl, suffix=True):
-    """
-    http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_13
-    """
-    max = False
-    pat = "".join(shl)
-    if pat[0] == ("%" if suffix else "#"):
-        max = True
-        pat = pat[1:]
-    size = len(subst)
-    indices = range(0, size)
-    if max != suffix:
-        indices = reversed(indices)
-    if suffix:
-        for i in indices:
-            if fnmatchcase(subst[i:], pat):
-                return subst[:i]
-        return subst
-    else:
-        for i in indices:
-            if fnmatchcase(subst[:i], pat):
-                return subst[i:]
-        return subst
-
-
-def remove_suffix(subst, shl):
-    return remove_affix(subst, shl, True)
-
-
-def remove_prefix(subst, shl):
-    return remove_affix(subst, shl, False)
-
-
-def follow_brace(shl, env):
-    param = next(shl)
-    if param == "#":
-        word = next(shl)
-        subst = env.get(word, "")
-        return str(len(subst))
-    subst = env.get(param, "")
-    param_set_and_not_null = bool(subst and (param in env))
-    param_set_but_null = bool((param in env) and not subst)
-    param_unset = param not in env
+def _version_from_importlib(name: str) -> Optional[str]:
     try:
-        modifier = next(shl)
-        if modifier == "%":
-            return remove_suffix(subst, shl)
-        elif modifier == "#":
-            return remove_prefix(subst, shl)
-        elif modifier == ":":
-            modifier = next(shl)
-            if modifier == "-":
-                word = next(shl)
-                if param_set_and_not_null:
-                    return subst
-                return word
-            elif modifier == "=":
-                word = next(shl)
-                if param_set_and_not_null:
-                    return subst
-                env[param] = word
-                return env[param]
-            elif modifier == "?":
-                if param_set_and_not_null:
-                    return subst
-                raise ParameterExpansionNullError(shl)
-            elif modifier == "+":
-                if param_set_and_not_null:
-                    return next(shl)
-                return subst  # ""
-            else:
-                raise ParameterExpansionParseError()
+        if sys.version_info >= (3, 8):
+            from importlib import metadata
         else:
-            if modifier == "-":
-                word = next(shl)
-                if param_unset:
-                    return word
-                return subst  # may be ""
-            elif modifier == "=":
-                word = next(shl)
-                if param_unset:
-                    env[param] = word
-                    return env[param]
-                return subst  # may be ""
-            elif modifier == "?":
-                if param_unset:
-                    msg = "".join(shl) or "parameter '$parameter' not found"
-                    raise ParameterExpansionNullError(msg)
-                return subst  # may be ""
-            elif modifier == "+":
-                word = next(shl)
-                if param_unset:
-                    return subst  # ""
-                return word
-            raise ParameterExpansionParseError()
-    except StopIteration:
-        return subst
+            import importlib_metadata as metadata
+    except ImportError:
+        return None
+
+    try:
+        return metadata.version(__name__)
+    except metadata.PackageNotFoundError:
+        return None
+
+
+def _version_from_pkg_resources(name: str) -> Optional[str]:
+    try:
+        import pkg_resources
+    except ImportError:
+        return None
+
+    try:
+        return pkg_resources.get_distribution(name).version
+    except pkg_resources.DistributionNotFound:
+        return None
+
+
+def _version_failover(name: str) -> str:
+    warnings.warn(
+        UserWarning(
+            f"Unable to determine version for {name}. Falling back to {_default_version}"
+        )
+    )
+    return _default_version
+
+
+__version__ = (
+    _version_from_pkg_resources(__name__)
+    or _version_from_importlib(__name__)
+    or _version_failover(__name__)
+)
